f12e508126a10e139d193750bd6eb13eb8e3383a
RadPaul <104913579+RadPaul@users.noreply.github.com>
2022-05-18 18:37:09 +0300

    Cleared repo

--- auth.php
+++
@@ -1,5 +0,0 @@
-<label for="login">Login:</label> 
-<input type="text" name="login" id="login" value="" placeholder="only letters and numbers" pattern="[a-zA-Z0-9]{4,20}" required autofocus>
-<label for="password">Password:</label>
-<input type="password" name="password" id="password" value="" placeholder="password" pattern="[a-zA-Z0-9]{4,20}" required>
-<input type="button" name="submit" id="submit" value="ok" onclick="authentification()">
